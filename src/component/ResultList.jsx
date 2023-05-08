d93a78dd640b2b4edc2e4c756733a1f0c2490d7a
khv2644511 <khv2644511@naver.com>
2023-05-08 17:00:01 +0900

    feat: ul 태그 내 불가능한 콘텐츠 제거

--- src/component/ResultList.jsx
+++ src/component/ResultList.jsx
@@ -3,13 +3,15 @@ import ResultItem from "./ResultItem";
 
 export default function ResultList({ result }) {
   return (
-    <ul className="bg-white mt-2 rounded-md p-4">
+    <div className="bg-white mt-2 rounded-md p-4">
       <h3 className="text-start text-gray-500">추천검색어</h3>
-      {result.length > 0 ? (
-        result.map((data) => <ResultItem key={data.id} data={data} />)
-      ) : (
-        <p>검색어 없음</p>
-      )}
-    </ul>
+      <ul className="">
+        {result.length > 0 ? (
+          result.map((data) => <ResultItem key={data.id} data={data} />)
+        ) : (
+          <p>검색어 없음</p>
+        )}
+      </ul>
+    </div>
   );
 }
